092ebf6820ce33196ae145e5adc8b5bc51c7d638
Hamza Wahab <hamzaabdulwahab19@gmail.com>
2024-06-03 19:24:27 +0500

    Enums are completed

--- Enums.cpp
+++ Enums.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 using namespace std;
+class TrafficLight {
+public:
+    enum Lights{Red, Yellow, Green};
+    // Lights ligh;
+    void Signal(int l) {
+         if(l == 0) {
+             cout<<"Red"<<endl;
+         }
+        else if(l == 1) {
+            cout<<"Yellow"<<endl;
+        }
+        else {
+            cout<<"Green"<<endl;
+        }
+    }
+};
 int main() {
-    enum gender {Male, Female};
+   TrafficLight signal;
 
+    for(int i = TrafficLight::Red; i <= TrafficLight::Green; i++) {
+        signal.Signal(i);
+        this_thread::sleep_for(chrono::seconds(5));
+    }
+    // sig.detach();
+    return 0;
 }
